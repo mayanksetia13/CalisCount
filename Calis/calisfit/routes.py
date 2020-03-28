43bc2a9c38d4e0f178cf9cb53e7a0214b15fec9f
Gagan Deep Singh <gagangulyanig@gmail.com>
2020-03-29 00:20:36 +0530

    Add Cred Class for BMR and Calories
    Add:
    + Storing Cred in DB after calculating stuff
    
    Update:
    + forms, User Class
    
    Fix:
    + Validation Error in registrater template

--- Calis/calisfit/routes.py
+++ Calis/calisfit/routes.py
@@ -1,10 +1,10 @@
-from flask import (render_template, url_for, flash, 
+from flask import (render_template, url_for, flash,
                    redirect, request)
 from calisfit import app, db, bcrypt
 from calisfit.forms import (RegistrationForm, LoginForm,
                             MyBodyForm, UpdateAccountForm)
-from calisfit.models import User
-from flask_login import (login_user, current_user, 
+from calisfit.models import User, Cred
+from flask_login import (login_user, current_user,
                          logout_user, login_required)
 
 
@@ -23,7 +23,9 @@ def register():
         hashed_password = bcrypt.generate_password_hash(
             form.password.data).decode('utf-8')
         user = User(username=form.username.data,
-                    email=form.email.data, password=hashed_password)
+                    gender=form.gender.data,
+                    email=form.email.data,
+                    password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash('Your account has been created!You are ready to workout', 'success')
@@ -43,7 +45,7 @@ def login():
                 login_user(user, remember=form.remember.data)
                 next_page = request.args.get('next')
                 return redirect(next_page) if next_page else redirect(url_for('about'))
-            flash('Login Unsuccessful. Please check your password!','danger')
+            flash('Login Unsuccessful. Please check your password!', 'danger')
         else:
             flash("Login Unsuccessful. Please Check Username", 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -59,6 +61,16 @@ def body():
     form = MyBodyForm()
     if request.method == 'POST':
         if form.validate_on_submit():
+            track = Cred(
+                user_id=current_user.id,
+                height=form.height.data,
+                weight=form.weight.data,
+                age=form.age.data,
+                activity=form.activity.data,
+            )
+            track.cal(gender=current_user.gender)
+            db.session.add(track)
+            db.session.commit()
             return redirect('track')
     return render_template('body.html', form=form)
 
@@ -75,24 +87,31 @@ def logout():
     flash('You have been logged out!', 'info')
     return redirect('/')
 
+
 @app.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
-	form = UpdateAccountForm()
-	if form.validate_on_submit():
-		current_user.username = form.username.data
-		current_user.email = form.email.data
-		db.session.commit()
-		flash('Your Profile has been updated','success')
-		return redirect(url_for('profile'))
-	elif request.method == 'GET':
-		form.username.data = current_user.username
-		form.email.data = current_user.email	
-	image_file = url_for('static',filename='profile_pics/'+ current_user.image_file)
-	return render_template('profile.html',title='Profile', image_file=image_file,form=form)
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your Profile has been updated', 'success')
+        return redirect(url_for('profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for(
+        'static', filename='profile_pics/' + current_user.image_file)
+    return render_template('profile.html', title='Profile', image_file=image_file, form=form)
+
 
 @app.route('/track')
 @login_required
 def trackrecord():
-	return render_template('track.html',title='TrackRecord')	
-
+    tracks = Cred.query.order_by(
+        db.desc('time')
+    ).filter_by(
+        user_id=current_user.id
+    ).all()
+    return render_template('track.html', title='TrackRecord')
