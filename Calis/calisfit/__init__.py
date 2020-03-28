dc04b89b1de27c8e6dbc686928535e7ffe350c5b
Gagan Deep Singh <gagangulyanig@gmail.com>
2020-03-29 01:52:02 +0530

    Fix Warning Messages
    For SQLALCHEMY_TRACK_MODIFICATIONS warning message at each time program starts

--- Calis/calisfit/__init__.py
+++ Calis/calisfit/__init__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 app.config['SECRET_KEY']='5462e873044b87176bfddd4951719708'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
